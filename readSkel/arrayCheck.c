e547dada8dcc26c44d5f17f73f7a24c501562e3e
Emily Hackett <emily.hcktt@gmail.com>
2017-01-18 12:30:52 +0800

    Magnus check

--- readSkel/arrayCheck.c
+++ readSkel/arrayCheck.c
@@ -6,6 +6,7 @@
 
 void ExampleSegment(NDskel *skl,int n)
 {
+	int i;
 	//	Prints to screen example of a NDskl_seg structure
 	//	so that data can be understood.
 	
@@ -17,7 +18,7 @@ void ExampleSegment(NDskel *skl,int n)
 
 	// I.e. points to a float that is position in segpos
 	printf("The %iD position coordinate of the segment is:\n",skl->ndims);
-	for(int i=0;i<skl->ndims;i++)
+	for(i=0;i<skl->ndims;i++)
 	{
 		printf(" pos[%i]		= %f\n",i,skl->Seg[n].pos[i]);
 	}
@@ -29,7 +30,7 @@ void ExampleSegment(NDskel *skl,int n)
 
 	// I.e. points to a double that is position in segdata
 	printf("The supplementary data for this segment is:\n");
-	for(int i=0;i<skl->nsegdata;i++)
+	for(i=0;i<skl->nsegdata;i++)
 	{
 		printf(" data[%i]	= %f\n",i,skl->Seg[n].data[i]);
 	}
@@ -39,13 +40,14 @@ void ExampleSegment(NDskel *skl,int n)
 
 void ExampleNode(NDskel *skl,int n)
 {
+	int i;
 	//	Prints to screen example of a NDskl_node structure
 	//	so that data can be understood
 
 	printf("EXAMPLE NODE: skl->Node[%i]\n",n);
 
 	printf("The %iD position coordinate of the node is:\n",skl->ndims);
-	for(int i=0;i<skl->ndims;i++)
+	for(i=0;i<skl->ndims;i++)
 	{
 		printf(" pos[%i]		= %f\n",i,skl->Node[n].pos[i]);
 	}
@@ -61,13 +63,13 @@ void ExampleNode(NDskel *skl,int n)
 
 	// Index over all connected arcs to find there number of segments
 	printf("The number of segments in each of the nnext Arcs is:\n");
-	for(int i=0;i<skl->Node[n].nnext;i++)
+	for(i=0;i<skl->Node[n].nnext;i++)
 	{
 		printf(" nsegs[%i]	= %i\n",i,skl->Node[n].nsegs[i]);
 	}
 	
 	printf("The supplementary data for this node is:\n");
-	for(int i=0;i<skl->nnodedata;i++)
+	for(i=0;i<skl->nnodedata;i++)
 	{
 		printf(" data[%i]	= %f\n",i,skl->Node[n].data[i]);
 	}
